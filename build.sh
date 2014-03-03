956c8143119a6af5dd498b7b2fcbf45039c69fc8
Simon Ruggier <Simon80@gmail.com>
2014-03-03 01:45:07 -0500

    Enable LTO on the main benchmark build
    This produces a small but noticeable speedup.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rustc --test -A dead-code -o UnicastThroughputTest_test UnicastThroughputTest.rs &&
-rustc  -A dead-code --opt-level 2 -g UnicastThroughputTest.rs
+rustc  -A dead-code --opt-level 2 -Z lto -g UnicastThroughputTest.rs
