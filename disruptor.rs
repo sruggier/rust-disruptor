85fa3967f879d866a28cc26434ecbbd1c805ee55
Simon Ruggier <Simon80@gmail.com>
2014-04-26 20:23:57 -0400

    Fix extern crate double linking issue
    This change is clearly a hack, because now disruptor depends on the
    calling module to import the crate. In the near future, I'll modularize
    the codebase and revisit this issue in the process.

--- disruptor.rs
+++ disruptor.rs
@@ -5,7 +5,6 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
-#[phase(syntax, link)] extern crate log;
 extern crate sync;
 extern crate time;
 use self::sync::raw::Mutex;
