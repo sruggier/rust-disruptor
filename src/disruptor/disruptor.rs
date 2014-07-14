5ef2522ee3303bded54d41bbca95b1f575d53576
Simon Ruggier <Simon80@gmail.com>
2014-07-13 21:18:59 -0400

    Replace crate_name directive instead of deprecated crate_id

--- src/disruptor/disruptor.rs
+++ src/disruptor/disruptor.rs
@@ -8,7 +8,7 @@
 
 // Copy rust-http's crate-id style for now. Until the API stabilizes, the versioning scheme merely
 // needs to convey that it is unstable.
-#![crate_id = "disruptor#0.1-pre"]
+#![crate_name = "disruptor"]
 #![desc = "Fast inter-thread communication for Rust"]
 // I used SPDX abbreviations here
 #![license = "MIT/Apache-2.0"]
