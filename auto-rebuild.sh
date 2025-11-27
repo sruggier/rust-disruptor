0308db868019efc1545cfc2a78c536d34aa66399
Simon Ruggier <Simon.Ruggier@gmail.com>
2025-11-26 20:25:14 -0500

    Format auto-rebuild.sh with shfmt

--- auto-rebuild.sh
+++ auto-rebuild.sh
@@ -5,24 +5,23 @@
 
 set -e
 
-function usage ()
-{
-    CMDNAME=`basename $0`
-    echo "usage:
+function usage() {
+	CMDNAME=$(basename $0)
+	echo "usage:
 $CMDNAME [-n]
 
 options:
     -u            Enable features that depend on unstable, namely benchmarking
 "
-    exit 2;
+	exit 2
 }
 
 UNSTABLE=0
 
 while getopts "u" ARG; do
 	case $ARG in
-		u)
-			UNSTABLE=1
+	u)
+		UNSTABLE=1
 		;;
 	esac
 done
@@ -31,16 +30,16 @@ SRC_DIR="$(dirname "${BASH_SOURCE[0]}")"
 cd "$SRC_DIR"
 
 while inotifywait -q -e attrib,close_write,create,delete,delete_self,move_self,moved_from,moved_to \
-		$(find -regex './\(Cargo.toml\|\(src\|examples\|tests\|benches\)/\(.*\.rs\)\)'); do
+	$(find -regex './\(Cargo.toml\|\(src\|examples\|tests\|benches\)/\(.*\.rs\)\)'); do
 
 	reset
-	echo -e "\n\nRebuilding...\n";
+	echo -e "\n\nRebuilding...\n"
 	(
 		cargo build &&
-		cargo test &&
-		if ((UNSTABLE)); then
-			cargo bench
-		fi &&
-		./target/debug/examples/unicast_throughput_benchmark
+			cargo test &&
+			if ((UNSTABLE)); then
+				cargo bench
+			fi &&
+			./target/debug/examples/unicast_throughput_benchmark
 	) || true
 done
