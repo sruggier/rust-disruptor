5136ad9c03e9908d54d4985dfc065dc7b99770b3
Simon Ruggier <Simon.Ruggier@gmail.com>
2026-06-26 19:25:27 -0400

    Update copyright notices

--- benches/latency.rs
+++ benches/latency.rs
@@ -1,3 +1,9 @@
+// Copyright 2026 Simon Ruggier.
+//
+// Licensed under the Apache License, Version 2.0
+// <https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <https://opensource.org/license/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
 use criterion::{
     Bencher, BenchmarkGroup, BenchmarkId, Criterion, criterion_group, criterion_main,
     measurement::WallTime,
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
-// Copyright 2014 Simon Ruggier.
+// Copyright 2026 Simon Ruggier.
 //
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
+// Licensed under the Apache License, Version 2.0
+// <https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <https://opensource.org/license/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
 
 #[macro_use]
 extern crate log;
